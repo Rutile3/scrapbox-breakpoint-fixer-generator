2a974dce638393108dbd988ce6e0dc61c3878ae8
Rutile3 <29337073+Rutile3@users.noreply.github.com>
2023-05-08 11:46:48 +0900

    📝 『ログ出力』のソースにコメントを追加

--- consoleLog.ts
+++ consoleLog.ts
@@ -1,13 +1,27 @@
+/** console.log() で出力する文字に色をつける制御文字 */
 class ConsoleColor {
+  /** 赤色 */
   static readonly RED = "\u001b[31m";
+
+  /** 緑色 */
   static readonly GREEN = "\u001b[32m";
+
+  /** リセット */
   static readonly RESET = "\u001b[0m";
 }
 
+/**
+ * 異常系のコンソール出力をします。
+ * @param message エラーメッセージ
+ */
 export function ErrorConsoleLog(message: string): void {
   console.log(ConsoleColor.RED + message + ConsoleColor.RESET);
 }
 
+/**
+ * 正常系のコンソール出力をします。
+ * @param message メッセージ
+ */
 export function SuccessConsoleLog(message: string): void {
   console.log(ConsoleColor.GREEN + message + ConsoleColor.RESET);
 }
